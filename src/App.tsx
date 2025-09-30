726dd155c61c719d412401a510a4425671b13679
LuisDevs <laldana161@gmail.com>
2025-09-30 17:25:45 -0600

    Initial commit: Tarot app with Vite + React + TS + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-6 bg-blue-100 rounded">
+      <h1 className="text-2xl font-bold text-blue-600">Tailwind 3 Works!</h1>
+    </div>
+  );
+}
+
+export default App;
